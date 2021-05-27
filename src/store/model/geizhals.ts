b3bc73daeda6be0e969b97747ed95268fe9deb30
unknown <matej@supersplet.net>
2021-05-27 12:18:53 +0200

    3070 geizhals update

--- src/store/model/geizhals.ts
+++ src/store/model/geizhals.ts
@@ -17,6 +17,146 @@ export const geizhals: Store = {
     },
   },
   links: [
+	{
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3070',
+      url:
+        'https://geizhals.at/zotac-gaming-geforce-rtx-3070-amp-holo-zt-a30700f-10p-a2452151.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/asus-rog-strix-geforce-rtx-3070-oc-a2406734.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/asus-tuf-gaming-geforce-rtx-3070-oc-a2406749.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'zotac',
+      model: 'twin edge oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/zotac-gaming-geforce-rtx-3070-twin-edge-oc-white-edition-zt-a30700j-10p-a2450371.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'gigabyte',
+      model: 'master',
+      series: '3070',
+      url:
+        'https://geizhals.at/gigabyte-aorus-geforce-rtx-3070-master-8g-gv-n3070aorus-m-8gd-a2406973.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'msi',
+      model: 'suprim',
+      series: '3070',
+      url:
+        'https://geizhals.at/msi-geforce-rtx-3070-suprim-8g-a2430309.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'evga',
+      model: 'ftw3 ultra',
+      series: '3070',
+      url:
+        'https://geizhals.at/evga-geforce-rtx-3070-ftw3-ultra-gaming-08g-p5-3767-kr-a2412839.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'evga',
+      model: 'xc3 ultra',
+      series: '3070',
+      url:
+        'https://geizhals.at/evga-geforce-rtx-3070-xc3-ultra-black-gaming-08g-p5-3755-kr-a2412824.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'kfa2',
+      model: 'ex oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/kfa-geforce-rtx-3070-ex-1-click-oc-37nsl6md1tbk-a2413075.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'inno3d',
+      model: 'ichill x4',
+      series: '3070',
+      url:
+        'https://geizhals.at/inno3d-geforce-rtx-3070-ichill-x4-c30704-08d6x-1710va35-a2408396.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'inno3d',
+      model: 'twin x2 oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/inno3d-geforce-rtx-3070-twin-x2-oc-n30702-08d6x-1710va32l-a2429903.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'msi',
+      model: 'x trio',
+      series: '3070',
+      url:
+        'https://geizhals.at/msi-geforce-rtx-3070-gaming-x-trio-a2364398.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'zotac',
+      model: 'twin edge oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/zotac-gaming-geforce-rtx-3070-twin-edge-oc-zt-a30700h-10p-a2406926.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'gigabyte',
+      model: 'oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/gigabyte-geforce-rtx-3070-gaming-oc-8g-gv-n3070gaming-oc-8gd-a2406936.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'asus',
+      model: 'dual fan',
+      series: '3070',
+      url:
+        'https://geizhals.at/asus-dual-geforce-rtx-3070-oc-a2366124.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/gigabyte-geforce-rtx-3070-vision-oc-8g-gv-n3070vision-oc-8gd-a2406952.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3070',
+      url:
+        'https://geizhals.at/msi-geforce-rtx-3070-suprim-x-8g-a2430243.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3070',
+      url:
+        'https://geizhals.at/palit-geforce-rtx-3070-gamingpro-oc-ne63070s19p2-1041a-a2381057.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'asus',
+      model: 'strix oc white',
+      series: '3070',
+      url:
+        'https://geizhals.at/asus-rog-strix-geforce-rtx-3070-oc-white-90yv0fr5-m0na00-a2446497.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 2x oc',
+      series: '3070',
+      url:
+        'https://geizhals.at/msi-geforce-rtx-3070-ventus-2x-oc-a2364400.html?hloc=at&hloc=de&hloc=eu&hloc=pl&hloc=uk',
+    },
     {
       brand: 'zotac',
       model: 'amp white',
